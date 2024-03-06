67184e4e1ab7aa7e2b6628a0c49b31ac712f343d
FilipeR13 <josefilipe.r.rodrigues13@gmail.com>
2024-03-06 23:26:37 +0000

    feat: Completed TPC4

--- TPC4/service.js
+++ TPC4/service.js
@@ -7,7 +7,8 @@ var axios = require('axios')
 const { parse } = require('querystring');
 
 var templates = require('./templates')
-var static = require('./static.js')
+var static = require('./static.js');
+const { symlinkSync } = require('fs');
 
 // Aux functions
 function collectRequestBodyData(request, callback) {
@@ -23,21 +24,6 @@ function collectRequestBodyData(request, callback) {
     else {
         callback(null);
     }
-}
-
-// Server creation
-async function getPeriodos(){
-    const set_periodos = new Set()
-    await axios.get("http://localhost:3000/compositores")
-    .then(response => {
-        response.data.forEach(compositor => {
-            set_periodos.add(compositor.periodo)
-        })
-    })
-    .catch(function(erro){
-        console.log("Erro: " + erro)
-    })
-    return set_periodos
 }
 
 async function getCompositoresByPeriodo(periodo){
@@ -97,10 +83,10 @@ var alunosServer = http.createServer((req, res) => {
                 }
             // GET /periodos --------------------------------------------------------------------
                 else if (req.url == "/periodos"){
-                    getPeriodos()
-                        .then(periodos => {
+                    axios.get("http://localhost:3000/periodos")
+                    .then(periodos => {
                             res.writeHead(200, {'Content-Type': 'html'})
-                            res.end(templates.periodosListPage(Array.from(periodos), d))
+                            res.end(templates.periodosListPage(periodos.data, d))
                         })
                         .catch(function(erro){
                             res.writeHead(200, {'Content-Type': 'text/html'})
@@ -108,6 +94,41 @@ var alunosServer = http.createServer((req, res) => {
                         })
                 }
             // GET /periodos/:id --------------------------------------------------------------------
+                else if (req.url == "/compositores/registo"){
+                    res.writeHead(200, {'Content-Type': 'text/html'})
+                    res.end(templates.compositorFormPage(d))
+                }
+                else if (req.url == "/periodos/registo"){
+                    console.log("entrou")
+                    res.writeHead(200, {'Content-Type': 'text/html'})
+                    res.write(templates.periodoFormPage(d))
+                    res.end()
+                }
+                else if (/\/periodos\/edit\/[A-Za-z]+/.test(req.url)){
+                    var periodo = req.url.split("/")[3]
+                    axios.get("http://localhost:3000/periodos/" + periodo)
+                        .then(response => {
+                            res.writeHead(200, {'Content-Type': 'text/html'})
+                            res.end(templates.periodoFormEditPage(response.data, d))
+                        })
+                        .catch(function(erro){
+                            res.writeHead(200, {'Content-Type': 'text/html'})
+                            res.end(templates.errorPage(erro, d))
+                        })
+                }
+                else if (/\/periodos\/delete\/[A-Za-z]+/.test(req.url)){
+                    var periodo = req.url.split("/")[3]
+                    axios.delete("http://localhost:3000/periodos/" + periodo)
+                        .then(response => {
+                            res.writeHead(200, {'Location': "/periodos"})
+                            res.write(templates.periodosListPage(response.data, d))
+                            res.end()
+                        })
+                        .catch(function(erro){
+                            res.writeHead(200, {'Content-Type': 'text/html'})
+                            res.end(templates.errorPage(erro, d))
+                        })
+                }
                 else if (/\/periodos\/[A-Za-z]+/.test(req.url)){
                     periodo = req.url.split("/")[2]
                     getCompositoresByPeriodo(periodo)
@@ -120,15 +141,103 @@ var alunosServer = http.createServer((req, res) => {
                             res.end(templates.errorPage(erro, d))
                         })
                 }
+                else if (/\/compositores\/edit\/C[0-9]+/.test(req.url)){
+                    var id = req.url.split("/")[3]
+                    axios.get("http://localhost:3000/compositores/" + id)
+                        .then(response => {
+                            res.writeHead(200, {'Content-Type': 'text/html'})
+                            res.end(templates.compositorFormEditPage(response.data, d))
+                        })
+                        .catch(function(erro){
+                            res.writeHead(200, {'Content-Type': 'text/html'})
+                            res.end(templates.errorPage(erro, d))
+                        })
+                }
+                else if (/\/compositores\/delete\/C[0-9]+/.test(req.url)){
+                    var id = req.url.split("/")[3]
+                    axios.delete("http://localhost:3000/compositores/" + id)
+                        .then(response => {
+                            res.writeHead(200, {'Location': "/compositores"})
+                            res.write(templates.compositorListPage(response.data, d))
+                            res.end()
+                        })
+                        .catch(function(erro){
+                            res.writeHead(200, {'Content-Type': 'text/html'})
+                            res.end(templates.errorPage(erro, d))
+                        })
+                }
                 else {
                     res.writeHead(404, {'Content-Type': 'text/html'})
                     res.end(templates.errorPage("Erro: " + req.url + " nao suportado...", d))
                 }
-
                 break
             case "POST":
-                res.writeHead(404, {'Content-Type': 'text/html'})
-                res.end(templates.errorPage("Erro: " + req.url + " nao suportado...", d))
+                // POST /compositores --------------------------------------------------------------------
+                if (req.url == "/compositores/registo"){
+                    collectRequestBodyData(req, result => {
+                        axios.post("http://localhost:3000/compositores", result)
+                            .then(response => {
+                                res.writeHead(200, {'Location': '/compositores'})
+                                res.write(templates.compositorListPage(response.data, d))
+                                res.end()
+                            })
+                            .catch(function(erro){
+                                res.writeHead(200, {'Content-Type': 'text/html'})
+                                res.end(templates.errorPage(erro, d))
+                            })
+                    })
+                }
+                else if (/\/compositores\/edit\/C[0-9]+/.test(req.url)){
+                    var id = req.url.split("/")[3]
+                    collectRequestBodyData(req, function(data){
+                        axios.put("http://localhost:3000/compositores/" + id, data)
+                            .then(response => {
+                                res.writeHead(200, {'Location' : '/compositores/' + id})
+                                res.end(templates.compositorPage(response.data, d))
+                            })
+                            .catch(function(erro){
+                                res.writeHead(200, {'Content-Type': 'text/html'})
+                                res.end(templates.errorPage(erro, d))
+                            })
+                    })
+                }
+                else if (req.url == "/periodos/registo"){
+                    collectRequestBodyData(req, result => {
+                        axios.post("http://localhost:3000/periodos", result)
+                            .then(response => {
+                                res.writeHead(200, {'Location': '/periodos'})
+                                res.write(templates.periodosListPage(response.data, d))
+                                res.end()
+                            })
+                            .catch(function(erro){
+                                res.writeHead(200, {'Content-Type': 'text/html'})
+                                res.end(templates.errorPage(erro, d))
+                            })
+                    })
+                }
+                else if (/\/periodos\/edit\/[A-Za-z]+/.test(req.url)){
+                    var id = req.url.split("/")[3]
+                    collectRequestBodyData(req, function(data){
+                        axios.put("http://localhost:3000/periodos/" + id, data)
+                            .then(response => {
+                                id = response.data.id
+                                getCompositoresByPeriodo(id)
+                                    .then(compositores => {
+                                        res.writeHead(200, {'Content-Type': 'text/html', 'Location' : '/periodos/' + id})
+                                        res.end(templates.periodoPage(id, compositores, d))
+                                    })
+                                    .catch(function(erro){
+                                        res.writeHead(200, {'Content-Type': 'text/html'})
+                                        res.end(templates.errorPage(erro, d))
+                                    })
+                            })
+                    })
+                }
+                else {  
+                    res.writeHead(404, {'Content-Type': 'text/html'})
+                    res.end(templates.errorPage("Erro: " + req.url + " nao suportado...", d))
+                }
+
             default: 
                 // Outros metodos nao sao suportados
         }
