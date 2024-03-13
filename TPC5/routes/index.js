da593282bb0d830b29795068909072f819f8dad4
FilipeR13 <josefilipe.r.rodrigues13@gmail.com>
2024-03-13 12:05:07 +0000

    feat: added pages to periodos

--- TPC5/routes/index.js
+++ TPC5/routes/index.js
@@ -2,6 +2,22 @@ var express = require('express');
 var router = express.Router();
 var axios = require('axios')
 
+async function getCompositoresByPeriodo(periodo){
+  const compositores = []
+  await axios.get("http://localhost:3000/compositores")
+  .then(response => {
+      response.data.forEach(compositor => {
+          if(compositor.periodo == periodo){
+              compositores.push(compositor)
+          }
+      })
+  })
+  .catch(function(erro){
+      console.log("Erro: " + erro)
+  })
+  return compositores
+}
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   var d = new Date().toISOString().substring(0,16)
@@ -28,7 +44,7 @@ router.get('/compositores/:id', function(req, res) {
   var d = new Date().toISOString().substring(0,16)
   axios.get('http://localhost:3000/compositores/' + req.params.id)
     .then( resposta => {
-      res.render('compositor', {compositor: resposta.data, data: d, title: 'Compositor'})
+      res.render('compositor', {compositor: resposta.data, data: d, title: resposta.data.nome})
     })
     .catch(erro => {
       res.render('error', {error: erro, message: 'Erro a mostrar compositor'})
@@ -46,26 +62,122 @@ router.get('/compositores/edit/:id', function(req, res) {
     })
 });
 
+router.get('/periodos', function(req, res) {
+  var d = new Date().toISOString().substring(0,16)
+  axios.get('http://localhost:3000/periodos')
+    .then(resposta => {
+      res.render('periodos', {periodos: resposta.data, data: d, title: 'Lista de periodos'})
+    })
+    .catch(erro => {
+      res.render('error', {error: erro, message: 'Erro na listagem de periodo'})
+    })
+});
+
+router.get('/periodos/registo', function(req, res) {
+  var d = new Date().toISOString().substring(0,16)
+  res.render('newPeriodo', {title: 'Registo de Periodo', data: d})
+});
+
+router.get('/periodos/:id', function(req, res) {
+  var d = new Date().toISOString().substring(0,16)
+  axios.get('http://localhost:3000/periodos/' + req.params.id)
+    .then( resposta => {
+      getCompositoresByPeriodo(req.params.id)
+        .then(compositores_list => {
+          res.render('periodo', {periodo: resposta.data, compositores: compositores_list, data: d, title: req.params.id})
+        })
+        .catch(erro => {
+          res.render('error', {error: erro, message: 'Erro a mostrar Periodo'})
+        })
+    })
+});
+
+
+router.get('/periodos/edit/:id', function(req, res) {
+  var d = new Date().toISOString().substring(0,16)
+  axios.get('http://localhost:3000/periodos/' + req.params.id)
+  .then( resposta => {
+    res.render('editPeriodo', {periodo: resposta.data, data: d, title: 'Editar Periodo'})
+  })
+  .catch(erro => {
+    res.render('error', {error: erro, message: 'Erro a editar Periodo'})
+  })
+});
+
+router.get('/compositores/delete/:id', function(req, res) {
+  var d = new Date().toISOString().substring(0,16)
+  axios.delete('http://localhost:3000/compositores/' + req.params.id)
+    .then( resposta => {
+      res.render('index', {title: 'Compositor eliminado', data: d})
+    })
+    .catch(erro => {
+      res.render('error', {error: erro, message: 'Erro ao eliminar compositor'})
+    })
+});
+
+router.get('/periodos/delete/:id', function(req, res) {
+  var d = new Date().toISOString().substring(0,16)
+  axios.delete('http://localhost:3000/periodos/' + req.params.id)
+    .then( resposta => {
+      res.render('index', {title: 'Periodo eliminado', data: d})
+    })
+    .catch(erro => {
+      res.render('error', {error: erro, message: 'Erro ao eliminar periodo'})
+    })
+});
+
 router.post('/compositores/registo', function(req, res) {
   var d = new Date().toISOString().substring(0,16)
   axios.post('http://localhost:3000/compositores', req.body)
     .then( resposta => {
-      res.render('confirmAluno', {info: req.body, title: 'Confirmação de Registo', data: d})
+      axios.get('http://localhost:3000/compositores')
+        .then(resposta => {
+          res.render('compositores', {compositores: resposta.data, title: 'Compositores', data: d})
+        })
+        .catch(erro => {
+          res.render('error', {error: erro, message: 'Erro ao gravar compositor'})
+        })
+  })
+});
+
+router.post('/periodos/registo', function(req, res) {
+  var d = new Date().toISOString().substring(0,16)
+  axios.post('http://localhost:3000/periodos', req.body)
+    .then( resposta => {
+      axios.get('http://localhost:3000/periodos')
+        .then(resposta => {
+          res.render('periodos', {periodos: resposta.data, title: 'Periodos', data: d})
+        })
+        .catch(erro => {
+          res.render('error', {error: erro, message: 'Erro ao gravar compositor'})
+        })	
     })
-    .catch(erro => {
-      res.render('error', {error: erro, message: 'Erro ao gravar aluno'})
-    })	
 });
 
 router.post('/compositores/edit/:id', function(req, res) {
   var d = new Date().toISOString().substring(0,16)
   axios.put('http://localhost:3000/compositores/' + req.params.id, req.body)
     .then( resposta => {
-      res.render('compositor', {info: req.body, title: 'Confirmação de Edição', data: d})
+      res.render('compositor', {compositor: req.body, title: req.body.nome, data: d})
     })
     .catch(erro => {
-      res.render('error', {error: erro, message: 'Erro ao editar aluno'})
+      res.render('error', {error: erro, message: 'Erro ao confirmar edicao de compositor'})
     })
 });
+
+router.post('/periodos/edit/:id', function(req, res) {
+  var d = new Date().toISOString().substring(0,16)
+  axios.put('http://localhost:3000/periodos/' + req.params.id, req.body)
+    .then( resposta => {
+      getCompositoresByPeriodo(req.params.id)
+        .then(compositores_list => {
+          res.render('periodo', {periodo: req.body, compositores: compositores_list, data: d, title: req.params.id})
+        })
+        .catch(erro => {
+          res.render('error', {error: erro, message: 'Erro a mostrar Periodo'})
+        })
+    })
+});
+
 
 module.exports = router;
